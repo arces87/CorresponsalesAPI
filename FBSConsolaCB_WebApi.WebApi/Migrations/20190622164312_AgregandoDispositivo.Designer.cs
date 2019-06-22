524cc84d73861834e7c6cc85720aa9e2dab38c01
lcarmenaty <leo.cty@gmail.com>
2019-06-22 13:01:54 -0400

    Añadiendo migracion para Dispositivo.

--- FBSConsolaCB_WebApi.WebApi/Migrations/20190622164312_AgregandoDispositivo.Designer.cs
+++ FBSConsolaCB_WebApi.WebApi/Migrations/20190622164312_AgregandoDispositivo.Designer.cs
@@ -4,14 +4,16 @@ using FBSConsolaCB_WebApi.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FBSConsolaCB_WebApi.WebApi.Migrations
 {
     [DbContext(typeof(FBSConsolaCBContext))]
-    partial class FBSConsolaCBContextModelSnapshot : ModelSnapshot
+    [Migration("20190622164312_AgregandoDispositivo")]
+    partial class AgregandoDispositivo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
