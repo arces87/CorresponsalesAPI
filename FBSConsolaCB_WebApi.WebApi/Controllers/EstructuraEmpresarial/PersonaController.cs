1d70c1696755149a035635a0424d56065d3e3628
Alfredo José Chiong Zaldivar <alfredj.chiong@gmail.com>
2019-07-08 17:57:37 -0400

    [DEL] Modificando el controller de Persona, eliminando los datos de corresponsales y supervisores

--- FBSConsolaCB_WebApi.WebApi/Controllers/EstructuraEmpresarial/PersonaController.cs
+++ FBSConsolaCB_WebApi.WebApi/Controllers/EstructuraEmpresarial/PersonaController.cs
@@ -25,77 +25,12 @@ namespace FBSConsolaCB_WebApi.WebApi.Controllers
             return await _servicio.List(filtro);
         }
 
-        [HttpPost("lista/corresponsales")]
-        public async Task<ActionResult<ModeloFuenteDatos<ModeloCorresponsal>>> ListaCorresponsales([FromBody] ModeloPaginacion filtro)
-        {
-            return await _servicio.ListaCorresponsales(filtro);
-        }
-
-        [HttpPost("lista/supervisores")]
-        public async Task<ActionResult<ModeloFuenteDatos<ModeloSupervisor>>> ListaSupervisores([FromBody] ModeloPaginacion filtro)
-        {
-            return await _servicio.ListaSupervisores(filtro);
-        }
-
-
+       
         [HttpGet("{id}")]
         public async Task<ActionResult<object>> Get(int Id)
         {
             var temp = await _servicio.Get(Id);
             return temp;
-        }
-
-        [HttpPost("corresponsal")]
-        public async Task<ActionResult<ModeloCorresponsal>> Create([FromBody] ModeloCorresponsal model)
-        {
-            var result = await _servicio.Create(model);
-
-            if (result != null)
-            {
-                return result;
-            }
-
-            throw new ApplicationException("INVALID_DATA_ATTEMPT");
-        }
-
-        [HttpPost("supervisor")]
-        public async Task<ActionResult<ModeloSupervisor>> Create([FromBody] ModeloSupervisor model)
-        {
-            var result = await _servicio.Create(model);
-
-            if (result != null)
-            {
-                return result;
-            }
-
-            throw new ApplicationException("INVALID_DATA_ATTEMPT");
-        }
-
-        [HttpPut("corresponsal")]
-        public async Task<ActionResult<ModeloCorresponsal>> Update([FromBody] ModeloCorresponsal model)
-        {
-            var result = await _servicio.Update(model);
-
-            if (result != null)
-            {
-                return result;
-            }
-
-            throw new ApplicationException("INVALID_DATA_ATTEMPT");
-        }
-
-
-        [HttpPut("supervisor")]
-        public async Task<ActionResult<ModeloSupervisor>> Update([FromBody] ModeloSupervisor model)
-        {
-            var result = await _servicio.Update(model);
-
-            if (result != null)
-            {
-                return result;
-            }
-
-            throw new ApplicationException("INVALID_DATA_ATTEMPT");
         }
 
         [HttpDelete("{id}")]
