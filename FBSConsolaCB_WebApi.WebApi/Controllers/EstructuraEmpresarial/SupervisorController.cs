6d74dadfb79bd123a2943415a3f8ab312b9ae823
Alfredo José Chiong Zaldivar <alfredj.chiong@gmail.com>
2019-07-08 17:36:24 -0400

    [MOD] Separando los controllers de supervisor y corresponsal

---
+++ FBSConsolaCB_WebApi.WebApi/Controllers/EstructuraEmpresarial/SupervisorController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using FBS.Dominio.Modelos.Filtro;
+using FBSConsolaCB_WebApi.Dominio.Modelos.EstructuraEmpresarial;
+using FBSConsolaCB_WebApi.Dominio.Servicios.Interfaces.EstructuraEmpresarial;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FBSConsolaCB_WebApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class SupervisorController : Controller
+    {
+        private readonly IServicioPersona _servicio;
+
+        public SupervisorController(IServicioPersona servicio)
+        {
+            _servicio = servicio;
+        }
+
+        [HttpPost("lista")]
+        public async Task<ActionResult<ModeloFuenteDatos<ModeloSupervisor>>> ListaSupervisores([FromBody] ModeloPaginacion filtro)
+        {
+            return await _servicio.ListaSupervisores(filtro);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ModeloSupervisor>> Create([FromBody] ModeloSupervisor model)
+        {
+            var result = await _servicio.Create(model);
+
+            if (result != null)
+            {
+                return result;
+            }
+
+            throw new ApplicationException("INVALID_DATA_ATTEMPT");
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ModeloSupervisor>> Update([FromBody] ModeloSupervisor model)
+        {
+            var result = await _servicio.Update(model);
+
+            if (result != null)
+            {
+                return result;
+            }
+
+            throw new ApplicationException("INVALID_DATA_ATTEMPT");
+        }
+    }
+}
