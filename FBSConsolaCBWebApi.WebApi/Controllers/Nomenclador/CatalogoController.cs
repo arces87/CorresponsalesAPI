72446089aa12018613f5761cbd5db583d2ab8fc0
Alfredo José Chiong Zaldivar <alfredj.chiong@gmail.com>
2019-07-28 12:46:25 -0400

    [FIX] Nombre de endopoint de lista de catalogo

--- FBSConsolaCBWebApi.WebApi/Controllers/Nomenclador/CatalogoController.cs
+++ FBSConsolaCBWebApi.WebApi/Controllers/Nomenclador/CatalogoController.cs
@@ -18,7 +18,7 @@ namespace FBSConsolaCBWebApi.WebApi.Controllers
             _mediador = mediador;
         }
 
-        [HttpGet("{tipo}", Name = "Catalogo_ListarCatalogos")]
+        [HttpGet(Name = "Catalogo_ListarCatalogos")]
         public async Task<ActionResult<ModeloObtenerListaCatalogo>> List(ObtenerListaCatalogoQuery model)
         {
             return await _mediador.Send(model);
