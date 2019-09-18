6f436791ee5a3f2b2f168e5702a92fa399998693
Alfredo José Chiong Zaldivar <alfredj.chiong@gmail.com>
2019-09-18 12:37:41 -0400

    [ADD] Identificacion al modelo de Agente

--- FBSConsolaCBWebApi.Dominio.Servicios/Agentes/Queries/ObtenerAgente/ObtenerAgenteMS.cs
+++ FBSConsolaCBWebApi.Dominio.Servicios/Agentes/Queries/ObtenerAgente/ObtenerAgenteMS.cs
@@ -6,7 +6,7 @@
         public string NombreAgente { get; set; }
 
         public string JsonAgente { get; set; }
-
+        public string Identificacion { get; set; }
         public string Ubicacion { get; set; }
         public string IdEstado { get; set; }
         public string NombreEstado { get; set; }
