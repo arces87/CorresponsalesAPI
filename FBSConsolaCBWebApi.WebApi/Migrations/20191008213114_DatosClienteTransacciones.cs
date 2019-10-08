4e609528c5df37e6dee94eaccb3889d54319009e
Alfred J. Chiong Zaldivar <alfredj.chiong@gmail.com>
2019-10-08 17:37:40 -0400

    [ADD] Migration of Transaction

---
+++ FBSConsolaCBWebApi.WebApi/Migrations/20191008213114_DatosClienteTransacciones.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FBSConsolaCBWebApi.WebApi.Migrations
+{
+    public partial class DatosClienteTransacciones : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IdentificacionCliente",
+                schema: "Corresponsales",
+                table: "Transaccion",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NombreCliente",
+                schema: "Corresponsales",
+                table: "Transaccion",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdentificacionCliente",
+                schema: "Corresponsales",
+                table: "Transaccion");
+
+            migrationBuilder.DropColumn(
+                name: "NombreCliente",
+                schema: "Corresponsales",
+                table: "Transaccion");
+        }
+    }
+}
