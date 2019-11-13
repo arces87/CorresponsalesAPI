a9f00e51a703b685a6f4c766e884d188a4492963
Alfred J. Chiong Zaldivar <alfredj.chiong@gmail.com>
2019-11-13 09:14:11 -0500

    [MOD] Agregando fecha y hora de la transaccion en el listar recaudaciones

--- FBSConsolaCBWebApi.Dominio.Servicios/Transacciones/Queries/ListarRecaudaciones/ModeloTransaccion.cs
+++ FBSConsolaCBWebApi.Dominio.Servicios/Transacciones/Queries/ListarRecaudaciones/ModeloTransaccion.cs
@@ -6,6 +6,8 @@ namespace FBSConsolaCBWebApi.Dominio.Servicios.Transacciones.Queries
     {
         public string NombreCliente { get; set; }
         public DateTime FechaSistema { get; set; }
+        public DateTime FechaDispositivo { get; set; }
+        public TimeSpan HoraDispositivo { get; set; }
         public double Valor { get; set; }
     }
 }
