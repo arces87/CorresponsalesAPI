28fc502095bd7683720a7ed5f69c7dc36154de6b
Alfred J. Chiong Zaldivar <alfredj.chiong@gmail.com>
2019-11-22 18:53:50 -0500

    [MOD] Adicionando comisiones a las recaudaciones

--- FBSConsolaCBWebApi.Dominio.Servicios/Transacciones/Queries/ListarRecaudaciones/ListarRecaudacionesHandler.cs
+++ FBSConsolaCBWebApi.Dominio.Servicios/Transacciones/Queries/ListarRecaudaciones/ListarRecaudacionesHandler.cs
@@ -2,6 +2,7 @@
 using FBS.Identidad.DAL.Modelado;
 using FBSConsolaCBWebApi.Infraestructure.Interfaces.Corresponsales;
 using MediatR;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -21,15 +22,22 @@ namespace FBSConsolaCBWebApi.Dominio.Servicios.Transacciones.Queries
             _mapper = mapper;
             _jsonConfiguracion = jsonConfiguracion;
         }
-
+        private double ContabilizarComisiones(ComisionPorTipoTransaccion comisiones)
+        {
+            var total = comisiones.AdministracionCanal.Value + comisiones.Agente.Value + comisiones.Cooperativa.Value + (comisiones.Facilito != null ? comisiones.Facilito.Value : 0);
+            return total;
+        }
         public async Task<ListarRecaudacionesMS> Handle(ListarRecaudacionesME request, CancellationToken cancellationToken)
         {
             var idDeposito = _jsonConfiguracion.Parametrizaciones.FirstOrDefault(p => p.Llave == "IdDeposito")?.Valor;
             var idRetiro = _jsonConfiguracion.Parametrizaciones.FirstOrDefault(p => p.Llave == "IdRetiro")?.Valor;
             var idCobroServicio = _jsonConfiguracion.Parametrizaciones.FirstOrDefault(p => p.Llave == "IdCobroServicio")?.Valor;
             var valorDeposito = 0.0;
+            var comisionDeposito = 0.0;
             var valorRetiro = 0.0;
+            var comisionRetiro = 0.0;
             var valorCobroServicio = 0.0;
+            var comisionCobroServicio = 0.0;
             var _retorno = new ListarRecaudacionesMS();
             _retorno.MontoCaja = await _repositorio.GetSaldoActual(request.IdAgente);
             if (idDeposito != null)
@@ -37,22 +45,25 @@ namespace FBSConsolaCBWebApi.Dominio.Servicios.Transacciones.Queries
 
                 var _model = await _repositorio.GetForTipo(idDeposito, request.IdAgente);
                 valorDeposito = _model.Sum(m => m.Valor);
+                comisionDeposito = _model.Sum(m => ContabilizarComisiones(JsonConvert.DeserializeObject<ComisionPorTipoTransaccion>(m.Comisiones)));
                 var lista = _model != null ? _mapper.Map<IEnumerable<ModeloTransaccion>>(_model) : new List<ModeloTransaccion>();
-                _retorno.Deposito = new ModeloListaRecaudaciones() { Total = valorDeposito, Lista = lista };
+                _retorno.Deposito = new ModeloListaRecaudaciones() { Total = valorDeposito, Comisiones = comisionDeposito, Lista = lista };
             }
             if (idRetiro != null)
             {
                 var _model = await _repositorio.GetForTipo(idRetiro, request.IdAgente);
                 valorRetiro = _model.Sum(m => m.Valor);
+                comisionRetiro = _model.Sum(m => ContabilizarComisiones(JsonConvert.DeserializeObject<ComisionPorTipoTransaccion>(m.Comisiones)));
                 var lista = _model != null ? _mapper.Map<IEnumerable<ModeloTransaccion>>(_model) : new List<ModeloTransaccion>();
-                _retorno.Retiro = new ModeloListaRecaudaciones() { Total = valorRetiro, Lista = lista };
+                _retorno.Retiro = new ModeloListaRecaudaciones() { Total = valorRetiro, Comisiones = comisionRetiro, Lista = lista };
             }
             if (idCobroServicio != null)
             {
                 var _model = await _repositorio.GetForTipo(idCobroServicio, request.IdAgente);
                 valorCobroServicio = _model.Sum(m => m.Valor);
+                comisionCobroServicio = _model.Sum(m => ContabilizarComisiones(JsonConvert.DeserializeObject<ComisionPorTipoTransaccion>(m.Comisiones)));
                 var lista = _model != null ? _mapper.Map<IEnumerable<ModeloTransaccion>>(_model) : new List<ModeloTransaccion>();
-                _retorno.CobroServicios = new ModeloListaRecaudaciones() { Total = valorCobroServicio, Lista = lista };
+                _retorno.CobroServicios = new ModeloListaRecaudaciones() { Total = valorCobroServicio, Comisiones = comisionCobroServicio, Lista = lista };
             }
             return _retorno;
         }
--- FBSConsolaCBWebApi.Dominio.Servicios/Transacciones/Queries/ListarRecaudaciones/ModeloListaRecaudaciones.cs
+++ FBSConsolaCBWebApi.Dominio.Servicios/Transacciones/Queries/ListarRecaudaciones/ModeloListaRecaudaciones.cs
@@ -5,6 +5,7 @@ namespace FBSConsolaCBWebApi.Dominio.Servicios.Transacciones.Queries
     public class ModeloListaRecaudaciones
     {
         public double Total { get; set; }
+        public double Comisiones { get; set; }
         public IEnumerable<ModeloTransaccion> Lista { get; set; }
     }
 }
