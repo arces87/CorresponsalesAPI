26d8d2024db5d2a27a55e89f39f698ef52a252c9
Alfred J. Chiong Zaldivar <alfredj.chiong@gmail.com>
2019-11-26 10:59:39 -0500

    [FIX] Direccion de endpoint de verificacion rol

--- FBSConsolaCBWebApi.WebApi/Controllers/Seguridad/RolesController.cs
+++ FBSConsolaCBWebApi.WebApi/Controllers/Seguridad/RolesController.cs
@@ -49,7 +49,7 @@ namespace FBSConsolaCBWebApi.WebApi.Controllers
             return await _mediador.Send(modelo);
         }
 
-        [HttpPost("verificarRol", Name = "Usuario_VerificarRol")]
+        [HttpPost("verificarRol", Name = "Rol_VerificarRol")]
         public async Task<ActionResult<bool>> VerificarRol ([FromBody] ComprobarRolME modelo)
         {
             return await _mediador.Send(modelo);
