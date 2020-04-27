30b27d452cb1ebccef5bb0a6b2c5fa5d92533f04
Pedro Luis Rodriguez Amador <rodriguezamador90@gmail.com>
2020-04-27 12:29:02 -0400

    [Api Mobil]
    Listar y Efectivisar Préstamos

---
+++ FBSMovilCBWebApi.Dominio.Servicios/Transacciones/Queries/ListarPrestamos/ListarPrestamosME.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using ServiciosFinancial.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FBSMovilCBWebApi.Dominio.Servicios.Transacciones.Queries.ListarPrestamos
+{
+    public class ListarPrestamosME: PorIdentificacionClienteActivaME, IRequest<InformacionPrestamosMSL>
+    {
+
+    }
+}
---
+++ FBSMovilCBWebApi.WebApi/Controllers/PrestamoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FBSMovilCBWebApi.Dominio.Servicios.Transacciones.Commands;
+using FBSMovilCBWebApi.Dominio.Servicios.Transacciones.Queries.ListarPrestamos;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ServiciosFinancial.Models;
+
+namespace FBSMovilCBWebApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    public class PrestamoController : Controller
+    {
+        private readonly IMediator _mediador;
+
+        public PrestamoController(IMediator mediador)
+        {
+            _mediador = mediador;
+        }
+
+        [HttpPost("listarPrestamos", Name = "Prestamos_Listar")]
+        [Produces(typeof(InformacionPrestamosMSL))]
+        public async Task<ActionResult<InformacionPrestamosMSL>> ListarPrestamos([FromBody] ListarPrestamosME modelo)
+        {
+            return await _mediador.Send(modelo);
+        }
+
+        [HttpPost("efectivizarPrestamos", Name = "Prestamos_Efectivizar")]
+        [Produces(typeof(EfectivizacionPrestamoMS))]
+        public async Task<ActionResult<EfectivizacionPrestamoMS>> EfectivizarPrestamos([FromBody] ProcesarAbonoPrestamoME modelo)
+        {
+            return await _mediador.Send(modelo);
+        }
+    }
+}
