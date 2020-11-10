a9a0d6890f66767a35bcaf862139c7899a8461a3
Ariel Céspedes <arielcespedes87@gmail.com>
2020-11-10 17:05:15 +0100

    Se incluyen parametros en el modelo de entrada de crear cliente

--- FBSMovilCBWebApi.Dominio.Servicios/Clientes/Commands/CrearCliente/CrearClienteME.cs
+++ FBSMovilCBWebApi.Dominio.Servicios/Clientes/Commands/CrearCliente/CrearClienteME.cs
@@ -16,8 +16,10 @@ namespace FBSMovilCBWebApi.Dominio.Servicios.Clientes.Commands
         public string TelefonoCelular { get; set; }
         public string DireccionDomiciliaria { get; set; }
         public string ReferenciaDomiciliaria { get; set; }
+        public string PaisNacimiento { get; set; }
+        public string EstadoCivil { get; set; }
+        public string CodigoDactilar { get; set; }
         public string Mail { get; set; }
-
         public string Usuario { get; set; }
         public string Imei { get; set; }
         public double Latitud { get; set; }
