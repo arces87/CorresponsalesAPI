01729b28557d01b6e0cd485ec4ba690bee6cad5e
Ariel Céspedes <arielcespedes87@gmail.com>
2021-01-22 08:38:04 +0100

    Prueba en la tabla de TransaccionRetiro

--- FBSMovilCBWebApi.Dominio.Servicios/Agente/SolicitarSaldoCuenta/SolicitarSaldoCuentaHandler.cs
+++ FBSMovilCBWebApi.Dominio.Servicios/Agente/SolicitarSaldoCuenta/SolicitarSaldoCuentaHandler.cs
@@ -1,5 +1,6 @@
 ﻿using FBS.Identidad.DAL.Modelado;
 using FBS.Infraestructura.Interfaces;
+using FBSConsolaCBWebApi.DAL.Corresponsales;
 using FBSConsolaCBWebApi.Infraestructure.Interfaces.Corresponsales;
 using FBSMovilCBWebApi.Dominio.Servicios.Logs.Commands;
 using FBSMovilCBWebApi.Dominio.Servicios.Usuarios.Commands.VerificarAgente;
@@ -51,29 +52,38 @@ namespace FBSMovilCBWebApi.Dominio.Servicios.Agente.SolicitarSaldoCuenta
 
         public async Task<double> Handle(SolicitarSaldoCuentaME request, CancellationToken cancellationToken)
         {
-            await _mediador.Send(new CrearLogME()
-            {
-                JsonLog = JsonConvert.SerializeObject(request),
-                IdTipoAccion = _jsonConfiguracion.Parametrizaciones.FirstOrDefault(p => p.Llave == "IdSolicitarSaldoCuenta").Valor,
-                IdEstado = _jsonConfiguracion.Parametrizaciones.FirstOrDefault(p => p.Llave == "IdLogSolicitado").Valor,
-            });
+            //await _mediador.Send(new CrearLogME()
+            //{
+            //    JsonLog = JsonConvert.SerializeObject(request),
+            //    IdTipoAccion = _jsonConfiguracion.Parametrizaciones.FirstOrDefault(p => p.Llave == "IdSolicitarSaldoCuenta").Valor,
+            //    IdEstado = _jsonConfiguracion.Parametrizaciones.FirstOrDefault(p => p.Llave == "IdLogSolicitado").Valor,
+            //});
 
-            await _mediador.Send(new VerificarAgenteME()
-            {
-                Usuario = request.Usuario,
-                Imei = request.Imei,
-                Mac = request.Mac,
-                Longitud = request.Longitud,
-                Latitud = request.Latitud
-            });
+            //await _mediador.Send(new VerificarAgenteME()
+            //{
+            //    Usuario = request.Usuario,
+            //    Imei = request.Imei,
+            //    Mac = request.Mac,
+            //    Longitud = request.Longitud,
+            //    Latitud = request.Latitud
+            //});
 
-            var agente = await _repositorioAgente.GetForId(_httpContext.HttpContext.User.Identity.Name);
-            var cuenta = await _repositorioCuenta.GetForAgente(agente.Id.ToString());
-            var apiKey = _apiKeyGenerator.generateApiKey(agente.Dispositivo.Imei);
-            var customHeaders = _apiKeyGenerator.generateCustomHeaders(apiKey);
-            DevuelveCuentaME cuentaAsociada = new DevuelveCuentaME() { SecuencialCuenta = int.Parse(cuenta.SecuencialCuenta) };
-            var respuestaCuentaAsociada = await _financialApi.Cuentas.DevuelveCuentaWithHttpMessagesAsync(cuentaAsociada, customHeaders);
-            return respuestaCuentaAsociada.Body.Saldo.Value;
+            //var agente = await _repositorioAgente.GetForId(_httpContext.HttpContext.User.Identity.Name);
+            //var cuenta = await _repositorioCuenta.GetForAgente(agente.Id.ToString());
+            //var apiKey = _apiKeyGenerator.generateApiKey(agente.Dispositivo.Imei);
+            //var customHeaders = _apiKeyGenerator.generateCustomHeaders(apiKey);
+            //DevuelveCuentaME cuentaAsociada = new DevuelveCuentaME() { SecuencialCuenta = int.Parse(cuenta.SecuencialCuenta) };
+            //var respuestaCuentaAsociada = await _financialApi.Cuentas.DevuelveCuentaWithHttpMessagesAsync(cuentaAsociada, customHeaders);
+            //return respuestaCuentaAsociada.Body.Saldo.Value;
+
+            var transaccionRetiro = new TransaccionRetiro()
+            {
+                IdTransaccion = Guid.Parse("C8650006-DF6A-4056-AC2E-08D85FC6FEF1"),
+                ValorCaja = 0,
+                FondoNegocio = 0
+            };
+            var idTransaccionRetiro = await _repositorioTransaccionRetiro.Add(transaccionRetiro);
+            return 3.2;
         }
     }
 }
