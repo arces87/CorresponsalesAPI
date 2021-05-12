a1386f448a2d1c6b0bfafb58e9b1503f71d7ce96
Ariel Céspedes <arielcespedes87@gmail.com>
2021-05-12 19:26:43 +0200

    Fixed: Verificar Agente.

--- FBSMovilCBWebApi.Dominio.Servicios/Usuarios/Commands/VerificarAgente/VerificarAgenteHandler.cs
+++ FBSMovilCBWebApi.Dominio.Servicios/Usuarios/Commands/VerificarAgente/VerificarAgenteHandler.cs
@@ -1,9 +1,11 @@
-﻿using FBS.Identidad.Dominio.Servicios.Canales.Queries;
+﻿using FBS.Identidad.DAL.Seguridad;
+using FBS.Identidad.Dominio.Servicios.Canales.Queries;
 using FBS.Infraestructura.Excepciones;
 using FBSConsolaCBWebApi.Infraestructure.Interfaces.Canales;
 using FBSConsolaCBWebApi.Infraestructure.Interfaces.Corresponsales;
 using MediatR;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Newtonsoft.Json;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,26 +15,30 @@ namespace FBSMovilCBWebApi.Dominio.Servicios.Usuarios.Commands.VerificarAgente
     public class VerificarAgenteHandler : IRequestHandler<VerificarAgenteME, bool>
     {
         private readonly IRepositorioAgente _repositorioAgente;
+        private readonly UserManager<Usuario> _manejadorUsuario;
         private readonly IRepositorioGeolocalizacion _repositorioGeolocalizacion;
         private readonly IHttpContextAccessor _httpContext;
 
-        public VerificarAgenteHandler(IRepositorioAgente repositorioAgente, IRepositorioGeolocalizacion repositorioGeolocalizacion, IHttpContextAccessor httpContext)
+        public VerificarAgenteHandler(IRepositorioAgente repositorioAgente, IRepositorioGeolocalizacion repositorioGeolocalizacion, IHttpContextAccessor httpContext, UserManager<Usuario> manejadorUsuario)
         {
             _repositorioAgente = repositorioAgente;
             _repositorioGeolocalizacion = repositorioGeolocalizacion;
-            _httpContext = httpContext;        
+            _httpContext = httpContext;
+            _manejadorUsuario = manejadorUsuario;
         }
         public async Task<bool> Handle(VerificarAgenteME request, CancellationToken cancellationToken)
         {
             var idUsuario = _httpContext.HttpContext.User.Identity.Name;
 
-            var agente = await _repositorioAgente.GetForUserName(idUsuario);
+            var usuario = await _manejadorUsuario.FindByIdAsync(idUsuario);
+
+            var agente = await _repositorioAgente.GetForUserName(request.Usuario);
                         
             var error = "Error en la validación de los datos de autenticación ";
 
             var jsonNegocio = JsonConvert.DeserializeObject<JsonNegocioMS>(agente.JsonAgente);
 
-            if (agente == null)
+            if (agente == null || usuario == null)
             {
                 throw new ExcepcionApp($"{error} | A001");
             }
