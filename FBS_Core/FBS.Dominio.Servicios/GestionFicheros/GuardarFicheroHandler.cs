789b2c7a4edd39c5756b8cc5a2d7ab12704d631d
Ariel Céspedes <arielcespedes87@gmail.com>
2021-05-12 17:51:46 +0200

    Fixed bug: Permisos sobre fichero que se suben al servidor.

--- FBS_Core/FBS.Dominio.Servicios/GestionFicheros/GuardarFicheroHandler.cs
+++ FBS_Core/FBS.Dominio.Servicios/GestionFicheros/GuardarFicheroHandler.cs
@@ -2,6 +2,8 @@
 using MediatR;
 using System;
 using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -16,25 +18,57 @@ namespace FBS.Dominio.Servicios.GestionFicheros
 
         public async Task<string> Handle(GuardarFicheroME request, CancellationToken cancellationToken)
         {
-            var _fileName = "";
-            try
+            var _extension = "." + request.Fichero.FileName.Split('.')[request.Fichero.FileName.Split('.').Length - 1];
+            var _fileName = Guid.NewGuid().ToString() + _extension;
+
+            var pathToSave = Path.Combine(request.DireccionGuardar, _fileName);
+            var mime = request.Fichero.ContentType;
+            if (!mime.Equals("image/png") && !mime.Equals("image/jpg") && !mime.Equals("image/jpeg"))
             {
-                var _extension = "." + request.Fichero.FileName.Split('.')[request.Fichero.FileName.Split('.').Length - 1];
-                _fileName = Guid.NewGuid().ToString() + _extension;
+                throw new ExcepcionApp("El tipo de fichero es inválido.");
+            }
 
-                var pathToSave = Path.Combine(request.DireccionGuardar, _fileName);
+            using (var bits = new FileStream(pathToSave, FileMode.Create))
+            {
 
-                using (var bits = new FileStream(pathToSave, FileMode.Create))
+                await request.Fichero.CopyToAsync(bits);
+            }
+
+            AplicarPermisos(pathToSave);
+                        
+            return _fileName;
+        }
+
+        private static void AplicarPermisos(string pathToSave)
+        {
+            var security = new FileSecurity(pathToSave,
+                          AccessControlSections.Owner |
+                          AccessControlSections.Group |
+                          AccessControlSections.Access);
+
+            var authorizationRules = security.GetAccessRules(true, true, typeof(NTAccount));
+
+            var owner = security.GetOwner(typeof(NTAccount));
+            foreach (AuthorizationRule rule in authorizationRules)
+            {
+                FileSystemAccessRule fileRule = rule as FileSystemAccessRule;
+                if (fileRule != null)
                 {
-                    await request.Fichero.CopyToAsync(bits);
+                    if (owner != null && fileRule.IdentityReference == owner)
+                    {
+                        if (fileRule.FileSystemRights.HasFlag(FileSystemRights.ExecuteFile) ||
+                           fileRule.FileSystemRights.HasFlag(FileSystemRights.ReadAndExecute) ||
+                           fileRule.FileSystemRights.HasFlag(FileSystemRights.FullControl))
+                        {
+                            var flags = FileSystemRights.Read | FileSystemRights.Modify | FileSystemRights.Delete;
+
+                            security.ModifyAccessRule(AccessControlModification.Add,
+                                new FileSystemAccessRule(owner, flags, AccessControlType.Allow),
+                                out bool modified);
+                        }
+                    }
                 }
             }
-            catch (Exception e)
-            {
-                throw new ExcepcionApp(e.Message);
-            }
-
-            return _fileName;
         }
     }
 }
