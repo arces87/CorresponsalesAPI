13916b7cfe7aae6a763a47feab2022745586b51a
Arces <arielcespedes87@gmail.com>
2022-03-07 08:58:45 -0600

    Se agregan logs en queries de pago

--- FBSMovilCBWebApi.Dominio.Servicios/PagoServicios/Queries/ObtenerFormatos/ObtenerFormatosHandler.cs
+++ FBSMovilCBWebApi.Dominio.Servicios/PagoServicios/Queries/ObtenerFormatos/ObtenerFormatosHandler.cs
@@ -8,6 +8,10 @@ using ServiciosFinancial.Models;
 using System.Threading;
 using System.Threading.Tasks;
 using FBS.Infraestructura.Interfaces;
+using FBS.Identidad.DAL.Modelado;
+using System.Linq;
+using Newtonsoft.Json;
+using FBSMovilCBWebApi.Dominio.Servicios.Logs.Commands;
 
 namespace FBSMovilCBWebApi.Dominio.Servicios.PagoServisios.Queries
 {
@@ -18,14 +22,16 @@ namespace FBSMovilCBWebApi.Dominio.Servicios.PagoServisios.Queries
         private readonly IMediator _mediador;
         private readonly IApiKeyGenerator _apiKeyGenerator;
         private readonly IRepositorioAgente _repositorioAgente;
+        private readonly IJsonConfiguracion _jsonConfiguracion;
 
-        public ObtenerFormatosHandler(IFBSCorresponsalesApi facilitoApi, IMapper mapper, IMediator mediador, IApiKeyGenerator apiKeyGenerator, IRepositorioAgente repositorioAgente)
+        public ObtenerFormatosHandler(IFBSCorresponsalesApi facilitoApi, IMapper mapper, IMediator mediador, IApiKeyGenerator apiKeyGenerator, IRepositorioAgente repositorioAgente, IJsonConfiguracion jsonConfiguracion)
         {
             _financialApi = facilitoApi;
             _mapper = mapper;
             _mediador = mediador;
             _apiKeyGenerator = apiKeyGenerator;
             _repositorioAgente = repositorioAgente;
+            _jsonConfiguracion = jsonConfiguracion;
         }
 
         public async Task<FormatoMS> Handle(ObtenerFormatosME request, CancellationToken cancellationToken)
@@ -44,7 +50,24 @@ namespace FBSMovilCBWebApi.Dominio.Servicios.PagoServisios.Queries
             var apiKey = _apiKeyGenerator.generateApiKey(agente.Dispositivo.Imei);
             var customHeaders = _apiKeyGenerator.generateCustomHeaders(apiKey);
 
+            var IdTipoAccion = _jsonConfiguracion.Parametrizaciones.FirstOrDefault(p => p.Llave == "IdCobroServicio").Valor;
+
+            await _mediador.Send(new CrearLogME()
+            {
+                JsonLog = JsonConvert.SerializeObject(request),
+                IdTipoAccion = IdTipoAccion,
+                IdEstado = _jsonConfiguracion.Parametrizaciones.FirstOrDefault(p => p.Llave == "IdLogEnviado").Valor,
+            });
+
             var respuesta = await _financialApi.PagoServiciosPagoAgil.FormatoWithHttpMessagesAsync(request, customHeaders);
+
+            await _mediador.Send(new CrearLogME()
+            {
+                JsonLog = JsonConvert.SerializeObject(respuesta.Body),
+                IdTipoAccion = IdTipoAccion,
+                IdEstado = _jsonConfiguracion.Parametrizaciones.FirstOrDefault(p => p.Llave == "IdLogRecibido").Valor,
+            });
+
             return respuesta.Body;
         }
     }
--- FBSMovilCBWebApi.Dominio.Servicios/PagoServicios/Queries/ObtenerServicios/ObtenerServiciosHandler.cs
+++ FBSMovilCBWebApi.Dominio.Servicios/PagoServicios/Queries/ObtenerServicios/ObtenerServiciosHandler.cs
@@ -7,6 +7,10 @@ using ServiciosFinancial.Models;
 using System.Threading;
 using System.Threading.Tasks;
 using FBS.Infraestructura.Interfaces;
+using FBSMovilCBWebApi.Dominio.Servicios.Logs.Commands;
+using Newtonsoft.Json;
+using FBS.Identidad.DAL.Modelado;
+using System.Linq;
 
 namespace FBSMovilCBWebApi.Dominio.Servicios.PagoServisios.Queries
 {
@@ -16,13 +20,15 @@ namespace FBSMovilCBWebApi.Dominio.Servicios.PagoServisios.Queries
         private readonly IMediator _mediador;
         private readonly IApiKeyGenerator _apiKeyGenerator;
         private readonly IRepositorioAgente _repositorioAgente;
+        private readonly IJsonConfiguracion _jsonConfiguracion;
 
-        public ObtenerServiciosHandler(IFBSCorresponsalesApi facilitoApi, IMediator mediador, IApiKeyGenerator apiKeyGenerator, IRepositorioAgente repositorioAgente)
+        public ObtenerServiciosHandler(IFBSCorresponsalesApi facilitoApi, IMediator mediador, IApiKeyGenerator apiKeyGenerator, IRepositorioAgente repositorioAgente, IJsonConfiguracion jsonConfiguracion)
         {
             _financialApi = facilitoApi;
             _mediador = mediador;
             _apiKeyGenerator = apiKeyGenerator;
             _repositorioAgente = repositorioAgente;
+            _jsonConfiguracion = jsonConfiguracion;
         }
 
         public async Task<ServiciosMSL> Handle(ObtenerServiciosME request, CancellationToken cancellationToken)
@@ -40,7 +46,16 @@ namespace FBSMovilCBWebApi.Dominio.Servicios.PagoServisios.Queries
             var agente = await _repositorioAgente.GetForUserName(request.Usuario);
             var apiKey = _apiKeyGenerator.generateApiKey(agente.Dispositivo.Imei);
             var customHeaders = _apiKeyGenerator.generateCustomHeaders(apiKey);
+
             var respuesta = await _financialApi.PagoServiciosPagoAgil.ServiciosWithHttpMessagesAsync(customHeaders);
+            var IdTipoAccion = _jsonConfiguracion.Parametrizaciones.FirstOrDefault(p => p.Llave == "IdCobroServicio").Valor;
+            await _mediador.Send(new CrearLogME()
+            {
+                JsonLog = JsonConvert.SerializeObject(respuesta.Body),
+                IdTipoAccion = IdTipoAccion,
+                IdEstado = _jsonConfiguracion.Parametrizaciones.FirstOrDefault(p => p.Llave == "IdLogRecibido").Valor,
+            });
+
             return respuesta.Body;
         }
     }
